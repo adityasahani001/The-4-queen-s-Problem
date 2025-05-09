6ef662e52dbb124091c0a9217ec3402af84b3533
Aditya Sahani <166254189+adityasahani001@users.noreply.github.com>
2025-05-09 23:29:50 +0530

    Solution_code.py

---
+++ python.py
@@ -0,0 +1,44 @@
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+    print("\n")
+
+
+def is_safe(board, row, col):
+    # Check the left side of the current row
+    for i in range(col):
+        if board[row][i] == 'Q':
+            return False
+
+    # Check the upper diagonal on the left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 'Q':
+            return False
+
+    # Check the lower diagonal on the left side
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 'Q':
+            return False
+
+    return True
+
+
+def solve_n_queens(board, col):
+    if col >= len(board):
+        print_board(board)
+        return True
+
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 'Q'
+            res = solve_n_queens(board, col + 1) or res
+            board[i][col] = '.'  # Backtrack
+
+    return res
+
+
+# Initialize 4x4 board
+n = 4
+board = [['.' for _ in range(n)] for _ in range(n)]
+solve_n_queens(board, 0)
